5e2b668cd5f5aa89f6e0a205a9e7bcf85bd84cec
RedNone <vasilevichnikolai@gmail.com>
2018-01-20 20:33:57 +0300

    added data layer

---
+++ app/src/main/java/com/example/rednone/androidmvpincludingmosby/DataLayer/Interfaces/JsonPlaceHolderApi.java
@@ -0,0 +1,35 @@
+package com.example.rednone.androidmvpincludingmosby.DataLayer.Interfaces;
+
+import com.example.rednone.androidmvpincludingmosby.DataLayer.Models.AlbumModel;
+import com.example.rednone.androidmvpincludingmosby.DataLayer.Models.CommentModel;
+import com.example.rednone.androidmvpincludingmosby.DataLayer.Models.PhotoModel;
+import com.example.rednone.androidmvpincludingmosby.DataLayer.Models.PostModel;
+import com.example.rednone.androidmvpincludingmosby.DataLayer.Models.UserModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by RedNone on 20.01.2018.
+ */
+
+public interface JsonPlaceHolderApi {
+
+    @GET("/posts")
+    Call<List<PostModel>> getPosts();
+
+    @GET("/comments")
+    Call<List<CommentModel>> getComments(@Query("postId") int postId);
+
+    @GET("/users")
+    Call<List<UserModel>> getUsers();
+
+    @GET("/albums")
+    Call<List<AlbumModel>> getAlbums();
+
+    @GET("/photos")
+    Call<List<PhotoModel>> getPhotos(@Query("albumId") int albumId);
+}
